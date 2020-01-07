7e3df7bb3c966834ebe6293755c332358bd7320d
Martin Oppitz <npmjs@martinoppitz.com>
2020-01-07 04:27:07 +0100

    update to latest deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-import buildOptimizer from 'rollup-plugin-angular-optimizer'
+import buildOptimizer from 'rollup-plugin-angular-optimizer';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import paths from 'rollup-plugin-paths';
 import pathMapping from 'rxjs/_esm5/path-mapping';
-import uglify from 'rollup-plugin-uglify';
+import uglify from 'rollup-plugin-uglify-es';
 
 export default {
   input: `./lib/src/hello-world.js`,
@@ -14,20 +14,20 @@ export default {
   },
   plugins: [
     paths(pathMapping()),
-    nodeResolve({jsnext: true, module: true}),
+    nodeResolve(),
     buildOptimizer(),
     uglify({
       mangle: true,
       compress: {
         global_defs: {
-          'ngDevMode': false,
+          ngDevMode: false
         },
         keep_fargs: false,
         passes: 3,
         pure_getters: true,
-        unsafe: true,
+        unsafe: true
       }
     })
   ],
   external: []
-}
+};
